6848356d4cec324ee60accf8e26d10cbf7c86a75
Jaisree Ramesh <jaisree.ramesh@gmail.com>
2021-12-10 12:01:43 +0100

    final commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import './App.css';
+import useIp from './useIp';
+import { Map, Marker } from "pigeon-maps";
+
+function App() {
+  const {
+    ip,
+    isp,
+    city,
+    country,
+    lat,
+    lng
+  } = useIp();
+  console.log(lat,lng);
+  console.log(country);
+  const c = (country.toLowerCase());
+  console.log(c)
+  const img = `https://flagcdn.com/${c}.svg`;
+  console.log(img)
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>FOUND YOUR IP</h1>
+        <div className="info-container">
+          <div className="row">
+              <Map height={475} defaultCenter={[ lat, lng ]} defaultZoom={9}>
+                <Marker width={50} anchor={[lat, lng ]} />
+              </Map>          
+          </div>
+          <div className="row">
+            <br /> IP : {ip} <br />
+            <br /> ISP : {isp} <br />
+            <br /> CITY : {city} <br />
+            <br /> COUNTRY : {country} <br />
+            <img src = {img} width = "425" height = "300"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/useIp.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+
+function useIp () {
+
+    const [items,setItems] = useState([{}])
+    // const [isLoading,setIsLoading] = useState([true])
+
+    useEffect(() =>  { 
+        fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}`)
+            .then((res) => res.json())
+            .then((json) => {
+            // setIsLoading(false);
+            setItems(json);
+            })
+            .catch(() => console.log("request failed"));
+        }, []);
+        const country = (items.location?.country);
+        // console.log("inside UseIP : ", country)
+        const city = items.location?.city;
+        const ip= items.ip;
+        const isp= items.isp;
+        const lat= items.location?.lat;
+        const lng= items.location?.lng;
+    
+    return {
+        ip, 
+        isp,
+        country,
+        city,
+        lat,
+        lng,  
+    };
+        
+}
+export default useIp;
